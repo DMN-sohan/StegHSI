5beefe8c6e3f08232c4beca30ce48f927e865294
Sohan <gundojusohan@gmail.com>
2024-06-30 02:11:29 +0530

    Commit

---
+++ kaggle_delete_dataset.py
@@ -0,0 +1,6 @@
+def main():
+  import shutil
+  shutil.rmtree('./data')
+
+if __name__ == '__main__':
+  main()
---
+++ kaggle_setup.py
@@ -0,0 +1,16 @@
+def main():
+  !git clone https://github.com/DMN-sohan/StegHSI.git
+  %cd StegHSI
+  !pip install -r requirements.txt
+  !kaggle datasets download -d matteocastrignano/mirflickr
+  import zipfile
+
+  with zipfile.ZipFile("mirflickr.zip", "r") as zip_ref:
+      zip_ref.extractall(".")
+  import os
+
+  os.rename('mirflickr25k', 'data')
+  os.remove("mirflickr.zip")
+
+if __name__ == '__main__':
+  main()
